2fe329707cfe9c44450f46b22387e19d5bc351d8
nami <nami.namazun@gmail.com>
2020-03-23 10:40:50 +0000

    orders全て,product_comment.phpno

--- cart/cart.php
+++ cart/cart.php
@@ -17,7 +17,7 @@ if(isset($_POST['delete'])){
 }
 
 $total = 0;
-if($_SESSION){
+if($_SESSION['cart']){
 	foreach($_SESSION['cart'] as $product){
 		$total += $product['price'] * $product['quantity'];
 	}
@@ -38,36 +38,41 @@ if($_SESSION){
 	<?php if(!empty($_SESSION['cart'])){ ?>
 		<h1>カート画面</h1>
 		<table border='1'>
-			<tr><td>Name</td><td>price</td><td>quantity</td><td></td><td></td></tr>
+			<tr><td class="column">Name</td><td class="column">price</td><td class="column">quantity</td><td class="column"></td><td class="column"></td></tr>
 			<?php foreach($_SESSION['cart'] as $product){ ?>
 			<tr>
-				<td><?=$product['name']?></td>
-				<td><?=$product['price']?></td>
-				<td><?=$product['quantity']?></td>
+				<td class="column"><?=$product['name']?></td>
+				<td class="column"><?=$product['price']?></td>
+				<td class="column"><?=$product['quantity']?></td>
 				<?php if($_SESSION['cart'][$product['name']]['quantity'] < 100){ ?>
-					<td><form action="" method="post">
-						<input type="submit" name="plus" value="+">
+					<td class="column"><form action="" method="post">
+						<input type="submit" name="plus" value="+" class="btn">
 						<input type="hidden" name="+" value="<?= $product['name'] ?>">
 					</form>
 				<?php } ?>
 				<?php if($_SESSION['cart'][$product['name']]['quantity'] > 1){ ?> 
 					<form action="" method="post">
-                                        	<input type="submit" name="minus" value="-">
+                                        	<input type="submit" name="minus" value="-" class="btn">
                                         	<input type="hidden" name="-" value="<?= $product['name'] ?>">
                                 	</form></td>
 				<?php } ?>
-				<td><form action="" method="post">
-					<input type="submit" value="削除" name="delete">
+				<td class="column"><form action="" method="post">
+					<input type="submit" value="削除" name="delete" class="btn">
 					<input type="hidden" name="name" value="<?= $product['name'] ?>">
 				</form></td>
 			</tr>
 			<?php } ?>
+			</table>
 		<p>合計金額：<?= $total ?> 円</p>
-		<a href="../orders/order_confirm.php">購入手続きに進む</a><br>
-		<a href="../products/product_list.php">商品一覧に戻る</a>
+		<div class="a_div">
+			<a href="../orders/order_confirm.php">購入手続きに進む</a>
+			<a href="../products/product_list.php">商品一覧に戻る</a>
+		</div>
 	<?php }else{ ?>
 		<p>カートの中に商品がありません</p>
-		<a href="../products/product_list.php">商品一覧に戻る</a>
+		<div class="a_div">
+			<a href="../products/product_list.php">商品一覧に戻る</a>
+		</div>
 	<?php } ?>
 	</div>
 </body>
